7ab5610726e61e98ca9643697eaaa76f64599a35
cwei <weiwu0937@gmail.com>
2021-12-23 17:59:46 +0800

    with nopass

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST','localhost');
-define('DB_USER','cwei');
-define('DB_PASS','sky365');
+define('DB_USER','noname');
+define('DB_PASS','nopass');
 define('DB_NAME','blog');
 
 $site_description = 'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.';
